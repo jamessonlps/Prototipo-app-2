f21736b9b9cbedac2080c885b637056bfa236a13
Henrique Silva <henriquegos@al.insper.edu.br>
2020-12-02 05:22:42 -0300

    pop up 1 concertado

--- js/contratar_saxofonista.js
+++ js/contratar_saxofonista.js
@@ -1,16 +1,16 @@
 function iniciaModal(modalID){
-    const modal2 = document.getElementById(modalID);
-    if(modal2){
-        modal2.classList.add("mostrar");
-        modal2.addEventListener("click", function() {
-            if (e.target.id == modalID || e.target.className == "fechar"){
+    const modal = document.getElementById(modalID);
+    if (modal) {
+        modal.classList.add("mostrar")
+        modal.addEventListener("click", (e) => {
+            if(e.target.id == modalID || e.target.className == "fechar"){
                 modal.classList.remove("mostrar");
             }
+            
         });
-        }
-    }
+}
+}
 
-const button = document.querySelector(".button");
-butones.addEventListener("click", function() {
-    iniciaModal(modal-concluido);
-});
+window.onload = function() {
+const butao = document.querySelector(".button");
+butao.addEventListener("click", () => iniciaModal("modal-concluido"))
